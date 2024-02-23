a57cfdb9d38984eeb8626ea1f4f9668e965564bd
Admin <dhanushindrakumar123@gmail.com>
2024-02-23 20:26:17 +0530

    Question 1 and 2 completed

---
+++ Question1.java
@@ -0,0 +1,9 @@
+public class Question1{
+    public static void main(String[] args) {
+        //program to print number from 10 to 50
+        for(int i=10;i<=50;i++){
+            System.out.println(i);//Prints the digits from 10 -50
+        }
+        
+    }
+}
